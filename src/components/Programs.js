3ef1b61af51f4e69f5bc402bce97c02009fa4955
Victor Gunnarsson <victor@educar.se>
2019-12-07 22:51:11 +0100

    Pontus lös

--- src/components/Programs.js
+++ src/components/Programs.js
@@ -4,10 +4,30 @@ import Program from './Program'
 import './style.css'
 
 export class Programs extends Component {
+  async getPrograms(section) {
+    console.log(section);
+    const data = await fetch(`http://localhost:3000/api/svt/program/${section}`);
+    const resp = await data.json();
+    return resp;
+}
+
+async getVideoId(id) {
+  const data = await fetch(`http://localhost:3000/api/svt/getVideoId/${id}`);
+  const resp = await data.json();
+  return resp;
+}
+
+async getm3u8Link(id) {
+  const data = await fetch(`http://localhost:3000/api/svt/m3u8/${id}`);
+  const resp = await data.json();
+  return resp;
+}
+
   render() {
+    const popularJson = this.getPrograms("populart")
     return <div className = "flex-container">
-      {Json.map((x) => (
-        <Program title={x[0]} thumbnail={x[1]} url={"https://svt-vod-9j.akamaized.net/d0/se/20191108/38320599-0021-4b80-8df5-070b08ce29c0/master.m3u8?alt=https%3A%2F%2Fswitcher.cdn.svt.se%2F38320599-0021-4b80-8df5-070b08ce29c0%2Fmaster.m3u8"}/>
+      {popularJson.program.map((x) => (
+        <Program title={x.title} thumbnail={x.thumbnail} url={this.getm3u8Link((a) => this.getVideoId(a))}/>
     ))}
     </div>
   }
