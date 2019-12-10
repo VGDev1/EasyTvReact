761ce69180cad1d7d5a7d2cb10a6028296fa962a
Victor Gunnarsson <victor@educar.se>
2019-12-10 21:19:40 +0100

    Working A_Ö

--- src/App.js
+++ src/App.js
@@ -13,14 +13,6 @@ class App extends Component {
     return (
     <Router>
         <div>
-          <h2>An amazing react page</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> Start </Link></li>
-            <li><Link to={'/programs/populart'} className="nav-link">Populart</Link></li>
-            <li><Link to={'/programs/letters'} className="nav-link">A-Ö</Link></li>
-          </ul>
-          </nav>
           <hr />
           <Switch>
               <Route exact path='/' component={Start} />
